e65e674f0bcb0b8c2f219d8b961aa8407fae133a
MitaliMDeshpande <100403268+MitaliMDeshpande@users.noreply.github.com>
2022-08-02 22:29:23 +0530

    Update Medicine.java

--- src/main/java/com/example/demo/Dao/Entity/Medicine.java
+++ src/main/java/com/example/demo/Dao/Entity/Medicine.java
@@ -7,6 +7,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import java.sql.Time;
+//import java.sql.Timestamp;
+ 
 
 @Entity
 @Table(name = "medicines")
@@ -16,24 +19,116 @@ public class Medicine {
 	long id;
 	@Column(name = "medName", nullable = false)
 	String medName;
-	@Column(name = "dosage", nullable = false)
-	String dosage;
-	@Column(name = "time", nullable = false)
-	String time;
-
+	@Column(name = "isSyrup")
+	String isSyrup;
+	@Column(name = "MorningDosage")
+	String MorningDosage;
+	@Column(name = "AfternoonDosage")
+	String AfternoonDosage;
+	@Column(name = "EveningDosage")
+	String EveningDosage;
+	@Column(name = "NightDosage")
+	String NightDosage;
+	@Column(name = "Morningtime")
+    Time MorningTiming ;
+	@Column(name = "Afternoontime")
+    Time AfternoonTiming ;
+	@Column(name = "Eveningtime")
+    Time EveningTiming ;
+	@Column(name = "Nighttime")
+    Time NightTiming ;
 	
 	public Medicine() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
+	
 
-	public Medicine(String medName, String dosage, String time) {
+
+	public Medicine(String medName, String isSyrup, String morningDosage, String afternoonDosage, String eveningDosage,
+			String nightDosage, Time morningTiming, Time afternoonTiming, Time eveningTiming, Time nightTiming) {
 		super();
 		this.medName = medName;
-		this.dosage = dosage;
-		this.time = time;
-
+		this.isSyrup = isSyrup;
+		MorningDosage = morningDosage;
+		AfternoonDosage = afternoonDosage;
+		EveningDosage = eveningDosage;
+		NightDosage = nightDosage;
+		MorningTiming = morningTiming;
+		AfternoonTiming = afternoonTiming;
+		EveningTiming = eveningTiming;
+		NightTiming = nightTiming;
+	}	
+	
+	
+	
+	public Time getMorningTiming() {
+		return MorningTiming;
 	}
+
+
+
+	public void setMorningTiming(Time morningTiming) {
+		MorningTiming = morningTiming;
+	}
+
+
+
+	public Time getAfternoonTiming() {
+		return AfternoonTiming;
+	}
+
+
+
+	public void setAfternoonTiming(Time afternoonTiming) {
+		AfternoonTiming = afternoonTiming;
+	}
+
+
+
+	public Time getEveningTiming() {
+		return EveningTiming;
+	}
+
+
+
+	public void setEveningTiming(Time eveningTiming) {
+		EveningTiming = eveningTiming;
+	}
+
+
+
+	public Time getNightTiming() {
+		return NightTiming;
+	}
+
+
+
+	public void setNightTiming(Time nightTiming) {
+		NightTiming = nightTiming;
+	}
+
+
+
+	public String getIsSyrup() {
+		return isSyrup;
+	}
+
+
+	public void setIsSyrup(String isSyrup) {
+		this.isSyrup = isSyrup;
+	}
+
+
+	public String getMorningDosage() {
+		return MorningDosage;
+	}
+
+
+	public void setMorningDosage(String morningDosage) {
+		MorningDosage = morningDosage;
+	}
+
 
 	public long getId() {
 		return id;
@@ -51,25 +146,37 @@ public class Medicine {
 		this.medName = medName;
 	}
 
-	public String getDosage() {
-		return dosage;
+
+	public String getMorningdosage() {
+		return MorningDosage;
 	}
 
-	public void setDosage(String dosage) {
-		this.dosage = dosage;
+	public void setMorningdosage(String morningdosage) {
+		MorningDosage = morningdosage;
 	}
 
-	public String getTime() {
-		return time;
+	public String getAfternoonDosage() {
+		return AfternoonDosage;
 	}
 
-	public void setTime(String time) {
-		this.time = time;
+	public void setAfternoonDosage(String afternoonDosage) {
+		AfternoonDosage = afternoonDosage;
 	}
 
-	@Override
-	public String toString() {
-		return "Medicine [id=" + id + ", medName=" + medName + ", dosage=" + dosage + ", time=" + time + "]";
+	public String getEveningDosage() {
+		return EveningDosage;
+	}
+
+	public void setEveningDosage(String eveningDosage) {
+		EveningDosage = eveningDosage;
+	}
+
+	public String getNightDosage() {
+		return NightDosage;
+	}
+
+	public void setNightDosage(String nightDosage) {
+		NightDosage = nightDosage;
 	}
 
 }
