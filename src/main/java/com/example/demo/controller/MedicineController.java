d6809bafba3cdc734733a079782390b5764a92a4
MitaliMDeshpande <100403268+MitaliMDeshpande@users.noreply.github.com>
2022-08-02 22:34:44 +0530

    Update MedicineController.java

--- src/main/java/com/example/demo/controller/MedicineController.java
+++ src/main/java/com/example/demo/controller/MedicineController.java
@@ -1,81 +1,56 @@
-package com.example.demo.Service;
-import java.util.*;
+package com.example.demo.controller;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Dao.Entity.Medicine;
-import com.example.demo.Repository.MedicineRepository;
-import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.Service.MedService;
 
-
-@Service
-public class MedServiceImpl implements MedService{
+@RestController
+@RequestMapping("api/v1/")
+public class MedicineController {
 	@Autowired
-	MedicineRepository medRepo;
-	
-	public MedServiceImpl(MedicineRepository medRepo) {
+	private MedService medservice;
+	public MedicineController(MedService medservice) {
 		super();
-		this.medRepo = medRepo;
+		this.medservice = medservice;
 	}
 	
-	@Override
-	public Medicine saveMed(Medicine medicine) {
-		// TODO Auto-generated method stub
-		return medRepo.save(medicine);
+	@GetMapping("medicines")
+	public List<Medicine> getAllMedicines(){
+		return this.medservice.getAllMedicines();
 	}
-
-	@Override
-	public List<Medicine> getAllMedicines() {
-		// TODO Auto-generated method stub
-		return medRepo.findAll();
-	}
-
-	@Override
-	public Medicine getMedById(long id) {
-		Optional<Medicine> med = medRepo.findById(id);
-		if(med.isPresent()) {
-			return med.get();
-		}else {
-			throw new ResourceNotFoundException("Medicine", "Id", id);
-		}
-		//return medRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Medicine", "Id", id));
-	}
-
-	@Override
-	public Medicine updateMed(Medicine medicine, long id) {
-		Optional<Medicine> med = medRepo.findById(id);
-		if(med.isPresent()) {
-			Medicine medicine1= med.get();
-			medicine1.setMedName(medicine.getMedName());
-			medicine1.setMorningdosage(medicine.getMorningdosage());
-			medicine1.setAfternoonDosage(medicine.getEveningDosage());
-			medicine1.setEveningDosage(medicine.getEveningDosage());
-			medicine1.setNightDosage(medicine.getNightDosage());
-			medicine1.setMorningTiming(medicine.getMorningTiming());
-			medicine1.setAfternoonTiming(medicine.getAfternoonTiming());
-			medicine1.setEveningTiming(medicine.getEveningTiming());
-			medicine1.setNightTiming(medicine.getNightTiming());
-			medRepo.save(medicine1);
-			return medicine1;
-		}else {
-			throw new ResourceNotFoundException("Medicine", "Id", id);
-		}
+	@PostMapping("medicines")
+	public Medicine createMedicine(@RequestBody Medicine medicines) {
+		return this.medservice.saveMed(medicines);
 		
 	}
-
-	@Override
-	public void deleteMed(long id) {
-		Optional<Medicine> med = medRepo.findById(id);
-		if(med.isPresent()) {
-			medRepo.deleteById(id);
-		}else {
-			throw new ResourceNotFoundException("Medicine", "Id", id);
-		}
-		
+	@GetMapping("medicines/{id}")
+	public ResponseEntity<Medicine> getMedicineById(@PathVariable("id") long id){
+		return new ResponseEntity<Medicine>(medservice.getMedById(id), HttpStatus.OK);
 	}
-
 	
+	@PutMapping("medicines/{id}")
+	public ResponseEntity<Medicine> updateMedicine(@PathVariable("id") long id, @RequestBody Medicine medicine){
 
+		return new ResponseEntity<Medicine>(medservice.updateMed(medicine, id), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("medicines/{id}")
+	public ResponseEntity<String> deleteMedicine(@PathVariable("id") long id){
+		medservice.deleteMed(id);
+		
+		return new ResponseEntity<String>("Medicine details deleted successfully!", HttpStatus.OK);
+	}
 }
